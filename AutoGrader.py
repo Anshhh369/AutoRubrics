020f777c13ca59318e7165535aa327c68eb24c93
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-07-04 13:26:24 -0400

    Update AutoGrader.py

--- AutoGrader.py
+++ AutoGrader.py
@@ -184,36 +184,36 @@ if st.button("Process Your Files",  help = "Click to process your file before as
         if "example_file" not in st.session_state:
             st.session_state.example_file = example_file(path)
 
-        options = select_option()
+options = select_option()
         
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
+if "messages" not in st.session_state:
+    st.session_state.messages = []
                 
-        # Display chat messages from history on app rerun
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
             
-        if query := st.chat_input("Ask your question here"):
-            # Display user message in chat message container
-            with st.chat_message("user"):
-                st.markdown(query)
-            # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": query})
+if query := st.chat_input("Ask your question here"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(query)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": query})
                     
-            # Get answer from retrieval chain
-            answer = get_answer(query)
-            result = answer["result"]
+    # Get answer from retrieval chain
+    answer = get_answer(query)
+    result = answer["result"]
             
-            # Display assistant response in chat message container
-            with st.chat_message("assistant"):
-                st.markdown(result)
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": result})
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(result)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": result})
                     
-            # Button to clear chat messages
-            def clear_messages():
-                st.session_state.messages = []
-            st.button("Clear", help = "Click to clear the chat", on_click=clear_messages)
+    # Button to clear chat messages
+    def clear_messages():
+        st.session_state.messages = []
+    st.button("Clear", help = "Click to clear the chat", on_click=clear_messages)
 
 
