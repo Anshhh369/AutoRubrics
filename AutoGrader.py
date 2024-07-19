66e081d34981a956f4a009053bcd2765fc96d38d
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-07-19 12:19:12 -0400

    Update AutoGrader.py

--- AutoGrader.py
+++ AutoGrader.py
@@ -316,14 +316,18 @@ if page == "Home":
 
     
 elif page == "Upload Document": 
-    if st.session_state.uploaded_files is None:
+    while st.session_state.uploaded_files is None:
         st.session_state.uploaded_files = st.file_uploader(
             "Upload your document", type=["txt"], accept_multiple_files=True
         )
-        if st.session_state.uploaded_files:
-            st.session_state.vector_store = example_file(st.session_state.uploaded_files)
-        else:
+        if st.session_state.uploaded_files is not None:
             st.write("Please upload a file first")
+            break
+
+
+    st.session_state.vector_store = example_file(st.session_state.uploaded_files)
+    
+            
 
 
         
