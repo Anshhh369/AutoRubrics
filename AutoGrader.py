59fb172cb4bbb9a86678165f295f40eb0f22b40c
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-07-21 15:04:02 -0400

    Update AutoGrader.py

--- AutoGrader.py
+++ AutoGrader.py
@@ -236,34 +236,33 @@ if page == "Home":
     st.session_state.selected_option = select_option()
         
     if st.session_state.selected_option:
-        if st.session_state.vector_store:
-            # Display chat messages from history on app rerun
-            for message in st.session_state.messages:
-                with st.chat_message(message["role"]):
-                    st.markdown(message["content"])
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
                                 
-            if query := st.chat_input("Ask your question here"):
-                # Display user message in chat message container
-                with st.chat_message("user"):
-                    st.markdown(query)
-                # Add user message to chat history
-                st.session_state.messages.append({"role": "user", "content": query})
+        if query := st.chat_input("Ask your question here"):
+            # Display user message in chat message container
+            with st.chat_message("user"):
+                st.markdown(query)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": query})
     
-                chat_history = format_chat_history(st.session_state.messages)
+            chat_history = format_chat_history(st.session_state.messages)
     
-                answer = get_answer(query)
-                        # break
+            answer = get_answer(query)
+
         
-                # Display assistant response in chat message container
-                with st.chat_message("assistant"):
-                    st.markdown(answer)
-                # Add assistant response to chat history                
-                st.session_state.messages.append({"role": "assistant", "content": answer})
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                st.markdown(answer)
+            # Add assistant response to chat history                
+            st.session_state.messages.append({"role": "assistant", "content": answer})
                                         
-                # Button to clear chat messages
-                def clear_messages():
-                    st.session_state.messages = []
-                st.button("Clear", help = "Click to clear the chat", on_click=clear_messages)
+            # Button to clear chat messages
+            def clear_messages():
+                st.session_state.messages = []
+            st.button("Clear", help = "Click to clear the chat", on_click=clear_messages)
 
 
 if page == "Upload Document":
