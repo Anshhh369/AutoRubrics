2cad12104744b5516b752ce827a62fb00847c0ce
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-07-22 13:03:11 -0400

    Update AutoGrader.py

--- AutoGrader.py
+++ AutoGrader.py
@@ -175,7 +175,7 @@ def get_answer(query):
     except:
         # pattern = r'text:'
         # answer = extract_information(answer, pattern)
-        response = chains.invoke({"query": query})
+        response = chains.invoke({"input": query})
         answer = response["result"]
         
 
