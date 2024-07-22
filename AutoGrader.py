8ea8ee68d3ee2394be2bdc1e060aad95057b0fec
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-07-22 10:58:31 -0400

    Update AutoGrader.py

--- AutoGrader.py
+++ AutoGrader.py
@@ -176,7 +176,7 @@ def get_answer(query):
     try:
         answer = answer['text']
     except:
-        answer = chains({"input": query, "options": st.session_state.selected_option, "context" : st.session_state.vector_store, "chat_history": chat_history})
+        answer = chains.run({"input": query, "options": st.session_state.selected_option, "context" : st.session_state.vector_store, "chat_history": chat_history})
         answer = answer['result']
         
     return answer
