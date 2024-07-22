8fd0bf165332d9a36a8ac150f1ea590c17070884
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-07-22 10:26:38 -0400

    Update AutoGrader.py

--- AutoGrader.py
+++ AutoGrader.py
@@ -176,7 +176,7 @@ def get_answer(query):
     try:
         answer = answer['text']
     except:
-        answer = answer['result']
+        answer = answer['answer']
         
     return answer
 
