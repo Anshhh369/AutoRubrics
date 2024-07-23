5c9d93ed8a14d640b5c1c29ee16d30f8ac8fd621
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-07-23 11:40:14 -0400

    Update AutoGrader.py

--- AutoGrader.py
+++ AutoGrader.py
@@ -162,35 +162,35 @@ def select_option():
     option_1 = st.selectbox(
         "Detail Level of Criteria",
         options_1,
-        index=options_1.index(st.session_state.selected_option[0]) if len(st.session_state.selected_option) > 0 else 0
+        index=options_1.index(st.session_state.selected_option[0]) if len(st.session_state.selected_option) > 0 else None
     )
     st.session_state.selected_option[0:1] = [option_1]
 
     option_2 = st.selectbox(
         "Grading Strictness",
         options_2,
-        index=options_2.index(st.session_state.selected_option[1]) if len(st.session_state.selected_option) > 1 else 0
+        index=options_2.index(st.session_state.selected_option[1]) if len(st.session_state.selected_option) > 1 else None
     )
     st.session_state.selected_option[1:2] = [option_2]
     
     option_3 = st.selectbox(
         "Area of Emphasis in Grading",
         options_3,
-        index=options_3.index(st.session_state.selected_option[2]) if len(st.session_state.selected_option) > 2 else 0
+        index=options_3.index(st.session_state.selected_option[2]) if len(st.session_state.selected_option) > 2 else None
     )
     st.session_state.selected_option[2:3] = [option_3]
 
     option_4 = st.selectbox(
         "Assignment Type",
         options_4,
-        index=options_4.index(st.session_state.selected_option[3]) if len(st.session_state.selected_option) > 3 else 0
+        index=options_4.index(st.session_state.selected_option[3]) if len(st.session_state.selected_option) > 3 else None
     )
     st.session_state.selected_option[3:4] = [option_4]
 
     option_5 = st.selectbox(
         "Assignment Style",
         options_5,
-        index=options_5.index(st.session_state.selected_option[4]) if len(st.session_state.selected_option) > 4 else 0
+        index=options_5.index(st.session_state.selected_option[4]) if len(st.session_state.selected_option) > 4 else None
     )
     st.session_state.selected_option[4:5] = [option_5]
 
