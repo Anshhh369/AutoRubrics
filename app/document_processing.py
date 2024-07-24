180c770523780bc95ea3f7266e866d6e1e35a20c
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-07-24 11:46:34 -0400

    Update document_processing.py

--- app/document_processing.py
+++ app/document_processing.py
@@ -5,6 +5,7 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader,Docx2txtLoader
 
 def assignment_file(uploaded_files):
     detector = chardet.UniversalDetector()
@@ -13,8 +14,41 @@ def assignment_file(uploaded_files):
         # Save file to a temporary directory
         temp_dir = tempfile.mkdtemp()
         path = os.path.join(temp_dir, uploaded_file.name)
+        
         with open(path, "wb") as f:
             f.write(uploaded_file.getvalue())
+
+        # Determine file extension
+        file_extension = uploaded_file.name.split(".")[-1].lower()
+        
+        # Load document based on its extension
+        if file_extension == "pdf":
+            loader = PyPDFLoader(path)
+        elif file_extension == "docx":
+            loader = Docx2txtLoader(path)
+        # elif file_extension == "pptx":
+        #     loader = UnstructuredPowerPointLoader(path)
+
+        # Load documents and split text
+        docs = loader.load()
+        
+        for doc in docs:
+            text = doc.page_content
+            st.write("file contents:", text)
+            
+
+
+        
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        documents = text_splitter.split_documents(docs)
+        db = Chroma.from_documents(documents, OpenAIEmbeddings())
+        
+    return db
+
+
+
+
+
         with open(path, "rb") as f:
             for line in f:
                 detector.feed(line)
@@ -23,7 +57,3 @@ def assignment_file(uploaded_files):
         detector.close()
         encoding = detector.result['encoding']
         raw_documents = TextLoader(path, encoding=encoding).load()
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        documents = text_splitter.split_documents(raw_documents)
-        db = Chroma.from_documents(documents, OpenAIEmbeddings())
-    return db
