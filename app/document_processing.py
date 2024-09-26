3ae6c39d50d4278e2b1df7a08d760ec7dc18535e
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-25 23:08:42 -0400

    Update document_processing.py

--- app/document_processing.py
+++ app/document_processing.py
@@ -65,7 +65,7 @@ def assignment_file(uploaded_files):
             azure_search_endpoint=vector_store_address,
             azure_search_key=vector_store_password,
             index_name=index_name,
-            api_version = "2024-06-01-preview",
+            api_version = "2020-08-01",
             embedding_function=OpenAIEmbeddings.embed_query,
             # Configure max retries for the Azure client
             additional_search_client_options={"retry_total": 4},
