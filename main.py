1a7d2741ea95302fb8ddb8bbb1fff987e77c83a6
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-28 15:39:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -76,8 +76,8 @@ if page == "Home":
 
             pattern = r"="
             
-            for line in st.session_state.messages:
-                search_result = re.search(pattern,line, re.DOTALL)
+            for k,v in st.session_state.messages.items():
+                search_result = re.search(pattern,v, re.DOTALL)
 
                 if search_result: 
                     result = search_result.group(1)
