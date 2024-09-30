7fb8ac7098ad6793edba7a9f43bda51af7c903f8
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-30 12:31:23 -0400

    Update chain_response.py

--- app/chain_response.py
+++ app/chain_response.py
@@ -81,7 +81,7 @@ def  get_chain(options,assignment,context,chat_history):
         retriever = AzureAISearchRetriever(
             content_key="content", 
             top_k=1, 
-            index_name="autorubrics-vectordb",
+            index_name="index_name",
         )
         chain = create_retrieval_chain(retriever, chain)
 
