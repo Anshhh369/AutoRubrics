6253b2262b19dc506f9b7bdaa26796ba64273142
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-10-01 02:21:11 -0400

    Update chain_response.py

--- app/chain_response.py
+++ app/chain_response.py
@@ -94,7 +94,7 @@ def get_answer(query):
         ans = response['answer']
         answer = ans['text']
 
-    pattern = r"^(.*?)=(.*)$"
+    pattern = r"^(.*=.*)$"
     for text in answer.splitlines():
         search_result = re.search(pattern, text)
         if search_result:
