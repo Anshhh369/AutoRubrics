946b6c64f1b9d0672f6ded037801890998ef3b51
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-10-01 00:42:21 -0400

    Update document_processing.py

--- app/document_processing.py
+++ app/document_processing.py
@@ -6,6 +6,7 @@ import pathlib
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import  RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import AzureSearch
+from langchain_community.retrievers import AzureAISearchRetriever
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader,Docx2txtLoader
 import pdfplumber
