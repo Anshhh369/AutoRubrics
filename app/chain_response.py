8b9e40ae2d5e813beeb6a90a3ad67c1d4c9824ae
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-10-01 02:50:44 -0400

    Update chain_response.py

--- app/chain_response.py
+++ app/chain_response.py
@@ -123,6 +123,8 @@ def get_answer(query):
                 )
                 
                 db_2 = vector_store_2.add_documents(documents)
+
+        return answer
                 
         
     return answer
