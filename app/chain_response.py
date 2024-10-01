2974c69fb6a76df203158eb0c17828849be791b4
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-10-01 02:45:30 -0400

    Update chain_response.py

--- app/chain_response.py
+++ app/chain_response.py
@@ -123,16 +123,7 @@ def get_answer(query):
                 )
                 
                 db_2 = vector_store_2.add_documents(documents)
-
-                if db_2:
-                    st.write("Final Rubric Submitted")
-
-                    break
-
-        break
                 
-    
-
         
     return answer
 
